91b25b7c3bc3bf361b89ef390ec88e2ec616c3e1
Wasif Nazeer <wasifnazeer11@gmail.com>
2023-10-03 04:09:37 +0500

    one file

--- bin/npxpackage.ts
+++ bin/npxpackage.ts
@@ -3,6 +3,42 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { NpxpackageStack } from '../lib/npxpackage-stack';
 
+
+import { execSync } from "child_process";
+
+const runCommand = (command:any)  => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (error) {
+    console.error(`Failed to execute ${command}`, error);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/Wasifnazeer11/awscdktemplate.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+
+const checkedOut = runCommand(gitCheckoutCommand);
+
+if (!checkedOut) {
+  process.exit(1);
+}
+
+console.log(`Installing dependencies for ${repoName}`);
+
+const installedDeps = runCommand(installDepsCommand);
+
+if (!installedDeps) {
+  process.exit(1);
+}
+
+console.log("Congrats ! You are ready. Follow the following commands to start");
+console.log(`cd ${repoName} && npm run dev`);
+
 const app = new cdk.App();
 new NpxpackageStack(app, 'NpxpackageStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
